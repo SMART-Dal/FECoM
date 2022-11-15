221a78957afd3ced751c5dbb07f063e72e74f7f6
Shang Ziyuan <happy.red.bear@gmail.com>
2022-11-15 10:30:01 -0400

    server energy measurement script

---
+++ energy_measurement.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -x
+
+# Collect the energy consumption of the GPU
+nvidia-smi -i 0 --loop-ms=1000 --format=csv,noheader --query-gpu=timestamp,power.draw > nvidia_smi.txt &
+
+# Get nvidia-smi's PID
+nvidia_smi_PID=$!
+
+# Trap: When the server is closed, terminate the nvidia-smi process
+exit_script() {
+    kill -9 "$nvidia_smi_PID"
+}
+
+trap exit_script SIGINT SIGTERM
+
+perf stat -I 1000 -e power/energy-pkg/,power/energy-ram/ -o perf.txt python3 dummy_serialisation/server.py | ts '[%Y-%m-%d %H:%M:%.S]'
