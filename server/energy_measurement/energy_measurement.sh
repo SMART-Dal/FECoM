05c3ed62f1b6deaf19e6d4ca9e77ef6aa0bcbd77
Tim Widmayer <tim.widmayer@Tims-MacBook-Pro.local>
2022-12-06 21:29:05 +0000

    move energy measurement scripts, add sample energy data files

---
+++ server/energy_measurement/energy_measurement.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -x
+
+# Set default value for count_interval_ms if undefined by user
+count_interval_ms=${1:-500}
+
+# Collect the energy consumption of the GPU
+nvidia-smi -i 0 --loop-ms=$count_interval_ms --format=csv,noheader --query-gpu=timestamp,power.draw > nvidia_smi.txt &
+
+# Get nvidia-smi's PID
+nvidia_smi_PID=$!
+
+# Trap: When the server is closed, terminate the nvidia-smi process
+exit_script() {
+    kill -9 "$nvidia_smi_PID"
+}
+
+trap exit_script SIGINT SIGTERM
+
+perf stat -I $count_interval_ms -e power/energy-pkg/,power/energy-ram/ -o perf.txt -x \; python3 server/server.py | ts '[%Y-%m-%d %H:%M:%.S]'
--- server/server.py
+++ server/server.py
@@ -3,14 +3,44 @@ Server to receive client requests to run ML methods and measure energy.
 """
 
 import numpy as np
+import time
+import statistics as stats
 import pickle
 from config import API_PATH, DEBUG, SERVER_HOST, SERVER_PORT
-from flask import Flask, Response, json, request
+from flask import Flask, Response, request
 
 
 app = Flask(__name__)
 
-def run_method(imports: str, function_to_run: str, args: list, kwargs: dict):
+def is_stable_state(max_wait_secs: int):
+    """
+    Return only when the system's energy consumption is stable.
+    """
+    # only consider the last n points
+    n = 50
+
+    for i in range(max_wait_secs*2):
+        data_lines = []
+        with open('energy_measurement/perf.txt', 'r') as f:
+            data_lines = f.read().splitlines(True)
+        
+        last_n_energies = [line.strip(' ').split(';')[1] for line in data_lines[-n:]]
+
+        energy_vars = [stats.variance(last_n_energies[n/5]) for i in range(n/5)]
+
+        with open('energy_measurement/perf.txt') as f:
+            for index, line in enumerate(f):
+                # ignore the first two lines
+                if index == 0 or index ==1:
+                    continue
+                line_items = line.strip(' ').split(';')[:-2]
+                print("Line {}: {}".format(index, line.strip()))
+        time.sleep(0.5)
+        
+
+    return False
+
+def run_method(imports: str, function_to_run: str, args: list, kwargs: dict, max_wait_secs: int):
     """
     Run the method given by function_to_run with the given arguments (args) and keyword arguments (kwargs).
     These two variables appear to not be used, however, they are used when evaluating the function_to_run
@@ -21,7 +51,11 @@ def run_method(imports: str, function_to_run: str, args: list, kwargs: dict):
     # (1) import relevant modules
     exec(imports)
 
-    # (2) evaluate the function return. This is where we should measure energy.
+    # # (2) continue only when the system has reached a stable state of energy consumption
+    # if not is_stable_state(max_wait_secs):
+    #     raise TimeoutError(f"System could not reach a stable state within {max_wait_secs} seconds")
+
+    # (3) evaluate the function return. This is where we should measure energy.
     func_return = eval(function_to_run)
 
     if DEBUG:
@@ -33,6 +67,7 @@ def run_method(imports: str, function_to_run: str, args: list, kwargs: dict):
 
 @app.route(API_PATH, methods=["POST"])
 def run_method_and_return_result():
+    MAX_WAIT_SECS = 100
     method_details = pickle.loads(request.data)
     
     if DEBUG:
@@ -42,7 +77,8 @@ def run_method_and_return_result():
         method_details["imports"],
         method_details["function"],
         method_details["args"],
-        method_details["kwargs"]
+        method_details["kwargs"],
+        MAX_WAIT_SECS
     )
 
     data = pickle.dumps(output)
