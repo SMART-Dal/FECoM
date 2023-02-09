618601ca894551bda262e1e08ab9b18907f6fd52
Saurabhsingh Rajput <saurabh@dal.ca>
2023-02-09 19:29:47 -0400

    change to single thread

--- client_agent/custom_method.py
+++ client_agent/custom_method.py
@@ -2,12 +2,12 @@ import pickle
 import requests
 import sys
 sys.path.append("../../../server")
-from send_request import send_request
+from send_request import send_request, send_single_thread_request
 # from dummy_send_request import dummy_send_request
 
 # TODO (by Tim) we also need to pass the wait_after_run_secs argument to send_request to specify
 # the number of seconds we want to wait after the function stops running on the server
 
 def custom_method(func,imports: str, function_to_run: str, method_object=None, function_args: list = None, function_kwargs: dict = None,max_wait_secs=0, custom_class=None):
-   result = send_request(imports=imports, function_to_run=function_to_run, function_args=function_args, function_kwargs=function_kwargs, max_wait_secs=max_wait_secs, method_object=method_object, custom_class=custom_class)
+   result = send_single_thread_request(imports=imports, function_to_run=function_to_run, function_args=function_args, function_kwargs=function_kwargs, max_wait_secs=max_wait_secs, method_object=method_object, custom_class=custom_class)
    return func
--- server/send_request.py
+++ server/send_request.py
@@ -115,4 +115,25 @@ def send_single_thread_request(imports: str, function_to_run: str, function_args
     if return_result:
         return pickle.loads(run_resp.content)
     else:
-        return run_resp.json()
+        result = pickle.loads(result)
+
+    try:
+        with open('methodcall-energy-dataset.json', 'r') as f:
+            existing_data = json.load(f)
+    except:
+        existing_data = []
+
+    if result:
+        try:
+            data = json.loads(result)
+            with open('methodcall-energy-dataset.json', 'w') as f:
+                existing_data.append(data)
+                json.dump(existing_data, f)
+        except json.decoder.JSONDecodeError as e:
+            print(f"Error decoding JSON: {e}")
+        except:
+            print("Error writing JSON to file")
+    else:
+        print("Response content is empty")
+    
+    return run_resp.json()
