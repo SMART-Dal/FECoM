f71fee1168a81752b12fb38effbeb5432c126aa9
Saurabhsingh Rajput <saurabh@dal.ca>
2023-03-19 19:00:57 -0300

    update file structure

---
+++ deprecated/create_experiments_subfolder.py
@@ -0,0 +1,25 @@
+import os
+import shutil
+import json
+
+source_dir = '/home/saurabh/code-energy-consumption/data/code-dataset/Patched-Repositories/tutorials'
+dest_dir = '/home/saurabh/code-energy-consumption/data/energy-dataset'
+
+# Recursively find all Jupyter notebooks in the source directory
+notebooks = []
+for root, dirs, files in os.walk(source_dir):
+    for file in files:
+        if file.endswith('.ipynb'):
+            notebooks.append(os.path.join(root, file))
+
+# Create a new subdirectory for each notebook in the destination directory
+for notebook in notebooks:
+    notebook_name = os.path.splitext(os.path.basename(notebook))[0]
+    dest_subdir = os.path.join(dest_dir, os.path.basename(os.path.dirname(notebook)), notebook_name)
+    os.makedirs(dest_subdir, exist_ok=True)
+    
+    # Create 10 JSON files in the new subdirectory
+    for i in range(1, 11):
+        experiment_file_path = os.path.join(dest_subdir, f'experiment-{i}.json')
+        with open(experiment_file_path, 'w') as f:
+            json.dump([], f)
