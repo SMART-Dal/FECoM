dc27a5e3002801ac1f339872dc39be508dfaa09b
Tim Widmayer <timw@cdr352.int.cedar.computecanada.ca>
2023-03-20 16:05:16 -0700

    properly pass wait_after_run_secs parameter to send_request

--- tool/client/custom_method.py
+++ tool/client/custom_method.py
@@ -21,7 +21,7 @@ experiment_file_name = os.path.splitext(file_name)[0]
 EXPERIMENT_FILE_PATH = EXPERIMENT_DIR /'method-level'/ immediate_folder / experiment_file_name / f'experiment-{experiment_number}.json'
 
 def custom_method(func,imports: str, function_to_run: str, method_object=None, function_args: list = None, function_kwargs: dict = None,max_wait_secs=MAX_WAIT_S, custom_class=None, wait_after_run_secs=WAIT_AFTER_RUN_S):
-   result = send_request(imports=imports, function_to_run=function_to_run, function_args=function_args, function_kwargs=function_kwargs, max_wait_secs=max_wait_secs, method_object=method_object, custom_class=custom_class,experiment_file_path=EXPERIMENT_FILE_PATH)
+   result = send_request(imports=imports, function_to_run=function_to_run, function_args=function_args, function_kwargs=function_kwargs, max_wait_secs=max_wait_secs, wait_after_run_secs=wait_after_run_secs, method_object=method_object, custom_class=custom_class,experiment_file_path=EXPERIMENT_FILE_PATH)
    return func
 
 if __name__ == "__main__":
