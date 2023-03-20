df70c6a7a616e54c6a27de82b0cb452c9bf6bae5
Tim Widmayer <timw@cdr352.int.cedar.computecanada.ca>
2023-03-20 13:25:21 -0700

    re-do changes accidently undone in commit 18ce02d

--- tool/experiment/experiments.py
+++ tool/experiment/experiments.py
@@ -11,7 +11,7 @@ import subprocess, os
 # base class that any Experiment subclass must implement
 # if there is shared code between experiments we can add it here as a method
 class Experiment(ABC):
-    def __init__(self, experiment_kind: str, project: str, output_dir: Path, code_dir: Path):
+    def __init__(self, experiment_kind: str, project: str, output_dir: Path, code_dir: Path, max_wait_secs: int, wait_after_run_secs: int):
         """
         args:
         - experiment_kind is a string that is hard-coded into the specific experiment implementation
@@ -19,18 +19,22 @@ class Experiment(ABC):
         - code_dir should most likely be set to client_config.CODE_DIR in 
         - output_dir should most likely be set to client_config.EXPERIMENT_DIR
         """
-        self.number = 0
+        # experiments start with 1
+        self.number = 1
         self.project = project
         self.code_dir = code_dir
+        self.max_wait_secs = max_wait_secs
+        self.wait_after_run_secs = wait_after_run_secs
         self.__output_dir = output_dir / experiment_kind / project
     
     # the output files are always in the same format, so this general formatter should work for any Experiment
     @property
     def output_file(self) -> Path:
         return self.__output_dir / f"experiment-{self.number}.json"
-
+    
+    # this method must increment number every time it is called
     @abstractmethod
-    def run(self, max_wait_secs: int, wait_after_run_secs: int):
+    def run(self):
         pass
 
     # do we need this?
@@ -39,13 +43,13 @@ class Experiment(ABC):
         pass
 
 class ProjectLevelExperiment(Experiment):
-    def __init__(self, project: str, experiment_dir: Path, code_dir: Path):
-        raise NotImplementedError("This has not been tested properly yet. Test before using.")
-        super().__init__("project-level", project, experiment_dir, code_dir)
+    def __init__(self, project: str, experiment_dir: Path, code_dir: Path, max_wait_secs: int, wait_after_run_secs: int):
+        # raise NotImplementedError("This has not been tested properly yet. Test before using.")
+        super().__init__("project-level", project, experiment_dir, code_dir, max_wait_secs, wait_after_run_secs)
         self.__code_file = self.code_dir / f"{self.project}_original.py"
         self.__code_string = None
 
-    def run(self, max_wait_secs: int, wait_after_run_secs: int):
+    def run(self):
         # have we already read the code file?
         if self.__code_string is None:
             self.__code_string = self.read_project_file(self.__code_file)
@@ -53,8 +57,8 @@ class ProjectLevelExperiment(Experiment):
         send_request(
             imports="",
             function_to_run=self.__code_string,
-            max_wait_secs=max_wait_secs,
-            wait_after_run_secs=wait_after_run_secs,
+            max_wait_secs=self.max_wait_secs,
+            wait_after_run_secs=self.wait_after_run_secs,
             experiment_file_path=self.output_file
             )
         self.number += 1
