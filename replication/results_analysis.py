a560c496f39647d0c537b751253700bcee6ed312
tim-w1d <tim.widmayer.20@ucl.ac.uk>
2023-04-14 16:31:12 +0100

    add replication code of first results for keras-classification and images-cnn

--- replication/results_analysis.py
+++ replication/results_analysis.py
@@ -7,9 +7,24 @@ if __name__ == "__main__":
     create_summary(data)
 
     project_name = "keras/classification"
-    data = init_project_energy_data(project_name, ExperimentKinds.PROJECT_LEVEL, first_experiment=1, last_experiment=5)
+    data = init_project_energy_data(project_name, ExperimentKinds.PROJECT_LEVEL, first_experiment=6, last_experiment=10)
     create_summary(data)
 
+    # TODO: currently experiments 1-5 didn't use the GPU (you can confirm this by comparing the
+    # summary statistics with those of experiments 6-10). We have to re-run these experiments
+    # project_name = "images/cnn"
+    # data = init_project_energy_data(project_name, ExperimentKinds.METHOD_LEVEL, first_experiment=1, last_experiment=5)
+    # create_summary(data)
+
     project_name = "images/cnn"
-    data = init_project_energy_data(project_name, ExperimentKinds.METHOD_LEVEL, first_experiment=1, last_experiment=5)
+    data = init_project_energy_data(project_name, ExperimentKinds.METHOD_LEVEL, first_experiment=6, last_experiment=10)
+    create_summary(data)
+
+    # TODO: see the todo above
+    # project_name = "images/cnn"
+    # data = init_project_energy_data(project_name, ExperimentKinds.PROJECT_LEVEL, first_experiment=1, last_experiment=5)
+    # create_summary(data)
+
+    project_name = "images/cnn"
+    data = init_project_energy_data(project_name, ExperimentKinds.PROJECT_LEVEL, first_experiment=6, last_experiment=10)
     create_summary(data)
